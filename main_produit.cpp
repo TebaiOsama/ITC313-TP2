95d046248db62037e3b964d3b7cb1a05450a2e8a
TebaiOsama <osama.tebai@gmail.com>
2019-12-06 09:46:19 +0100

    Q2

---
+++ main_produit.cpp
@@ -0,0 +1,24 @@
+/** 
+  * File:     main_produit.cpp 
+  * Author:   O.Tebai (Osama_Tebai@etu.u-bourgogne.fr)
+  * Date:     Fall 2019 
+  * Course:   C-C++ Programming / Esirem 3A Informatique Electronique 
+  * Summary:  Testing of class Produit
+  */
+
+#include <iostream>
+#include "Produit.h"
+
+std::ostream& operator<<(std::ostream& t_flux, Produit& t_produit){
+	t_flux<<"ID : "<<t_produit.getIdProduit()<<std::endl
+		  <<"Titre Produit : "<<t_produit.getTitreProduit()<<std::endl
+	  	  <<"Description Produit : "<<t_produit.getDescriptionProduit()<<std::endl
+		  <<"Quantite Disponible : "<<t_produit.getQuantiteDisponible()<<std::endl
+		  <<"Prix Produit : "<<t_produit.getPrixProduit()<<std::endl;
+	return t_flux;
+}
+
+int main(){
+	Produit Switch(4,"Nintendo",54);
+	std::cout<<Switch;
+}
