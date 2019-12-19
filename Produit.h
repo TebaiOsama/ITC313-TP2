e62c8cedf3f9528bd1406d3dac805f382ff43040
TebaiOsama <osama.tebai@gmail.com>
2019-12-19 15:45:07 +0100

    Q3

--- Produit.h
+++ Produit.h
@@ -6,32 +6,27 @@
   * Summary:  Declaration of the class Produit
   */
 
-#ifndef _produit_h
-#define _produit_h
+#ifndef _Produit_h
+#define _Produit_h
 
 #include <string>
 
 class Produit{
 private:
-	int m_id_produit;
 	std::string m_titre_produit;
 	std::string m_description_produit;
 	int m_quantite_disponible;
 	double m_prix_produit;
 public:
-	Produit(int t_id_produit, std::string t_titre_produit, double t_prix_produit, std::string t_description_produit="", int t_quantite_disponible=0);
+	Produit();
+	Produit(std::string t_titre_produit, double t_prix_produit, int t_quantite_disponible=1, std::string t_description_produit="");
 	//getters
-	int getIdProduit() const;
 	std::string getTitreProduit() const;
 	std::string getDescriptionProduit() const;
 	int getQuantiteDisponible() const;
 	double getPrixProduit() const;
 	//setters
 	void setQuantiteDisponible(int t_quantite_disponible);
-	//void setTitreProduit(std::string t_titre_produit);
-	//void setDescriptionProduit(std::string t_description_produit);
-	//void setPrixProduit(double t_prix_produit);
-	//methodes
 };
 
 #endif
---
+++ main_magasin.cpp
@@ -0,0 +1,30 @@
+/** 
+  * File:     main_magasin.cpp 
+  * Author:   F.Forrer/O.Tebai (Francois_Forrer@etu.u-bourgogne.fr/Osama_Tebai@etu.u-bourgogne.fr)
+  * Date:     Fall 2019 
+  * Course:   C-C++ Programming / Esirem 3A Informatique Electronique 
+  * Summary:  Testing of class Magasin
+  */
+
+#include "Produit.h"
+//#include "Client.h"
+//#include "Commande.h"
+#include "Magasin.h"
+#include <string>
+#include <vector>
+
+int main(){
+
+	std::vector<Produit*> produits;
+	Produit p("PS4", 450, 200, "Console Sony");
+	produits.push_back(&p);
+
+	produits.push_back(new Produit("Switch", 300, 150, "Console Nintendo"));
+	produits.push_back(new Produit("Xbox", 350, 450, "Console Microsoft"));
+
+
+	Magasin micro(produits);
+
+	micro.affichageProduits();
+	return 0;
+}
